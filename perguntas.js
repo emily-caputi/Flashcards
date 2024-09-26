90c06be233f54dd9fbdb460926ed95147c7f6704
Emily <132068134+emily-caputi@users.noreply.github.com>
2024-09-26 08:38:01 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    "Programação",
-    "O que e'JavaScript? ",
-    "O JavaScript  é uma linguagem e programação."
+    'Programação',
+    'O que e JavaScript?',
+    'O JavaScript  é uma linguagem e programação.'
 )
 
 criaCartao(
-    "Geografia",
-    "Qual é a capital da França?",
-    "A capital da França é Paris"
+    'Geografia',
+    'Qual é a capital da França?',
+    'A capital da França é Paris'
 )
 
 criaCartao(
-    "Programação",
-    "O que é uma Função",
-    "Uma função é um blocoe coigo que executa alguma tarefa"
+    'Programação',
+    'O que é uma Função',
+    'Uma função é um blocoe coigo que executa alguma tarefa'
 )
 
 criaCartao(
-    caategoria,
-    pergunta,
-    resposta
+    'Lingua Inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em inglês é HI (RAI).'
 )
